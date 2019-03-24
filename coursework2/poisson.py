c4b060f33429f3ba297192f0286d4c917504a32c
dominic <dominicrawlins98@gmail.com>
2019-03-24 19:02:41 +0000

    finished all 3 questions- may not be right

--- coursework2/poisson.py
+++ coursework2/poisson.py
@@ -21,6 +21,26 @@ def get_spike_train(rate,big_t,tau_ref):
 
 
 
+def fanoFactor(spikeTrain, timePeriod, totalTime):
+    numberOfBins = int(totalTime/timePeriod)
+    spikePeriodBins = [0]*numberOfBins
+    for spike in spikeTrain:
+        binNumber = spike  // timePeriod
+        spikePeriodBins[int(binNumber)] += 1
+
+    mean = len(spikeTrain) / numberOfBins
+
+    varianceSum = 0
+    for bin in spikePeriodBins:
+        varianceSum  += (bin - mean) ** 2
+
+    variance = varianceSum / numberOfBins
+    fanoFactorReturn = variance / mean
+
+    return fanoFactorReturn
+
+
+
 Hz=1.0
 sec=1.0
 ms=0.001
@@ -32,6 +52,72 @@ big_t=5*sec
 
 spike_train=get_spike_train(rate,big_t,tau_ref)
 
-print(len(spike_train)/big_t)
+#print(len(spike_train)/big_t)
 
-print(spike_train)
+#print(spike_train)
+firingRate = 35
+refractoryPeriods = [0, 5]
+fanoFactorWindows = [10, 50, 100]
+time = 1000*sec
+
+print("----------------------\n\nQuestion 1\n\n------------------------\n")
+
+
+
+for refractoryPeriod in refractoryPeriods:
+    for fanoFactorWindow in fanoFactorWindows:
+        tau = refractoryPeriod * ms
+        spikeTrain = get_spike_train(firingRate, time, tau)
+        window = fanoFactorWindow * ms
+        fanoFactorResult = fanoFactor(spikeTrain, window, time)
+        print("Refractory Period: ", refractoryPeriod)
+        print("Window: ", window)
+        print("Fano Factor: ", fanoFactorResult, "\n")
+
+
+print("\n\n----------------------\n\nQuestion 2\n\n------------------------\n")
+
+def load_data(filename,T):
+
+    data_array = [T(line.strip()) for line in open(filename, 'r')]
+
+    return data_array
+
+
+#spikes=[int(x) for x in load_data("rho.dat")]
+spikes=load_data("rho.dat",int)
+
+spikeTrain = []
+
+for idx, spike in enumerate(spikes):
+    if(spike == 1):
+        spikeTrain.append(idx*2*ms)
+
+fanoFactorResult = fanoFactor(spikeTrain, 2*ms, 20*60)
+print("Fano Factor: ", fanoFactorResult)
+
+
+
+
+print("\n\n----------------------\n\nQuestion 3\n\n------------------------\n")
+
+#stimulus=[float(x) for x in load_data("stim.dat")]
+stimulus=load_data("stim.dat",float)
+
+print(len(stimulus))
+averages=[]
+totalTime = 20*60
+windowTime = 100*ms
+totalWindows =  totalTime / windowTime/2
+for window in range(int(totalWindows)):
+    spikeStimulus = []
+    start = window * 50
+    length = 50
+    for indiSpike in range(length):
+        if(spikes[indiSpike + start] == 1):
+            spikeStimulus.append(stimulus[indiSpike + start])
+    if(len(spikeStimulus) == 0):
+        averages.append(0)
+    else:
+        averageStimulus = sum(spikeStimulus)/ len(spikeStimulus)
+        averages.append(averageStimulus)
